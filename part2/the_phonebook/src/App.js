1db81db7b4a74b34d0a952de2fafa7bbbe873677
Luis Rodriguez <lrodrigueza9719@gmail.com>
2023-05-01 19:20:08 -0400

    The Phonebook, step 4

--- part2/the_phonebook/src/App.js
+++ part2/the_phonebook/src/App.js
@@ -6,6 +6,7 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
   const [phoneNumber, setPhoneNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -35,9 +36,21 @@ const App = () => {
     setPhoneNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const personsToShow = filter !== '' 
+  ? persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
+  : persons;
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with<input value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -51,7 +64,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <>
-        {persons.map((person) => (
+        {personsToShow.map((person) => (
           <>
             <p key={person.name}>{person.name} {person.number}</p>
           </>
